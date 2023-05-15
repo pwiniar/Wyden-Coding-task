6a060a0bd01b30ff4d07d0887acddb6378ca3489
Pawel <winiarskipawel@gmail.com>
2023-05-15 08:24:18 +0200

    add custom json serializer

--- src/test/java/wyden/coding/task/infrastructure/ExampleMessages.java
+++ src/test/java/wyden/coding/task/infrastructure/ExampleMessages.java
@@ -10,9 +10,10 @@ class ExampleMessages {
 
     static class BondTicker {
         static BookTicker getDefault() {
-            return BookTicker.of("BTCUSDT", new BigDecimal("26919.40000000"),
-                    new BigDecimal("0.02926000"), new BigDecimal("26919.41000000"),
-                    new BigDecimal("1.83654000"));
+            return BookTicker.of("BTCUSDT", Price.of(new BigDecimal("26919.40000000")),
+                    Price.of(new BigDecimal("0.02926000")),
+                    Price.of(new BigDecimal("26919.41000000")),
+                    Price.of(new BigDecimal("1.83654000")));
         }
     }
 
